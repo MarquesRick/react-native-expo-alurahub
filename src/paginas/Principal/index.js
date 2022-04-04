1a1594e9ba9886327f0de0689dbf22a2d877ea84
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-04 20:04:59 -0300

    config prettier and eslint

--- src/paginas/Principal/index.js
+++ src/paginas/Principal/index.js
@@ -1,50 +1,59 @@
 import React, { useState } from 'react';
-import { Text, View, Image, TouchableOpacity, TextInput, Alert, ScrollView } from 'react-native';
+import {
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+  ScrollView,
+} from 'react-native';
 import estilos from './estilos';
 
 export default function Principal({ navigation }) {
-    const [nomeUsuario, setNomeUsuario] = useState('');
-    const [usuario, setUsuario] = useState({});
+  const [nomeUsuario, setNomeUsuario] = useState('');
+  const [usuario, setUsuario] = useState({});
 
-    return (
-        <ScrollView>
-            <View style={estilos.container}>
-                <>
-                    <View style={estilos.fundo} />
-                    <View style={estilos.imagemArea}>
-                        <Image source={{ uri: 'https://avatars.githubusercontent.com/u/54721131?v=4' }} style={estilos.imagem} />
-                    </View>
-                    <Text style={estilos.textoNome}>Nome do usuario</Text>
-                    <Text style={estilos.textoEmail}>Email do usuario</Text>
-                    <View style={estilos.seguidoresArea}>
-                        <View style={estilos.seguidores}>
-                            <Text style={estilos.seguidoresNumero}>30</Text>
-                            <Text style={estilos.seguidoresTexto}>Seguidores</Text>
-                        </View>
-                        <View style={estilos.seguidores}>
-                            <Text style={estilos.seguidoresNumero}>40</Text>
-                            <Text style={estilos.seguidoresTexto}>Seguindo</Text>
-                        </View>
-                    </View>
-                    <TouchableOpacity onPress={() => navigation.navigate('Repositorios')}>
-                        <Text style={estilos.repositorios}>
-                            Ver os repositórios
-                        </Text>
-                    </TouchableOpacity>
-                </>
-
-                <TextInput
-                    placeholder="Busque por um usuário"
-                    autoCapitalize="none"
-                    style={estilos.entrada}
-                />
-
-                <TouchableOpacity style={estilos.botao}>
-                    <Text style={estilos.textoBotao}>
-                        Buscar
-                    </Text>
-                </TouchableOpacity>
+  return (
+    <ScrollView>
+      <View style={estilos.container}>
+        <>
+          <View style={estilos.fundo} />
+          <View style={estilos.imagemArea}>
+            <Image
+              source={{
+                uri: 'https://avatars.githubusercontent.com/u/37425086?s=96&v=4',
+              }}
+              style={estilos.imagem}
+            />
+          </View>
+          <Text style={estilos.textoNome}>Nome do usuario</Text>
+          <Text style={estilos.textoEmail}>Email do usuario</Text>
+          <View style={estilos.seguidoresArea}>
+            <View style={estilos.seguidores}>
+              <Text style={estilos.seguidoresNumero}>30</Text>
+              <Text style={estilos.seguidoresTexto}>Seguidores</Text>
             </View>
-        </ScrollView>
-    );
+            <View style={estilos.seguidores}>
+              <Text style={estilos.seguidoresNumero}>40</Text>
+              <Text style={estilos.seguidoresTexto}>Seguindo</Text>
+            </View>
+          </View>
+          <TouchableOpacity onPress={() => navigation.navigate('Repositorios')}>
+            <Text style={estilos.repositorios}>Ver os repositórios</Text>
+          </TouchableOpacity>
+        </>
+
+        <TextInput
+          placeholder="Busque por um usuário"
+          autoCapitalize="none"
+          style={estilos.entrada}
+        />
+
+        <TouchableOpacity style={estilos.botao}>
+          <Text style={estilos.textoBotao}>Buscar</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 }
